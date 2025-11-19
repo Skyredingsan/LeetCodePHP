827d0fde840fbb57c3c55a62f561b3b21b851a63
Skyredingsan <Skyreding@mail.ru>
2025-11-19 19:26:37 +0300

    Solve for add two numbers

---
+++ 9 Palindrome Number/Palindrome Number.php
@@ -0,0 +1,28 @@
+<?php
+
+class Solution {
+    function isPalindrome($x) {
+        if ($x < 0) {
+            return false;
+        }
+
+        if ($x % 10 == 0 && $x != 0) {
+            return false;
+        }
+
+        if ($x == 0){
+            return true;
+        }
+
+        $original = $x;
+        $reverved = 0;
+
+        while ($x > 0) {
+            $digit = $x % 10;
+            $reserved = $reserved * 10 + $digit;
+            $x = (int)($x/10);
+        }
+
+        return $original === $reserved;
+    }
+}
